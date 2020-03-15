0f91bae7a42553a3294c21cdf50bac78c719801a
unknown <mdnislam502@gmail.com>
2020-03-16 00:08:12 +0600

    Updates

---
+++ precache-manifest.2f84d82ebf8105b0eabd187c7ba61153.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8fb079e6b83c8fe01432f5e467c5f6",
+    "url": "/Users-Profile-Collection-/index.html"
+  },
+  {
+    "revision": "afb462a33235ca730870",
+    "url": "/Users-Profile-Collection-/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e775e36814b98bc4680c",
+    "url": "/Users-Profile-Collection-/static/css/main.0ea40815.chunk.css"
+  },
+  {
+    "revision": "afb462a33235ca730870",
+    "url": "/Users-Profile-Collection-/static/js/2.c88d6324.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Users-Profile-Collection-/static/js/2.c88d6324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e775e36814b98bc4680c",
+    "url": "/Users-Profile-Collection-/static/js/main.693f879b.chunk.js"
+  },
+  {
+    "revision": "f6ec87539810f397ff5b",
+    "url": "/Users-Profile-Collection-/static/js/runtime-main.f05e8152.js"
+  },
+  {
+    "revision": "8c08a809e884e82c21abd3e1ddd79ee2",
+    "url": "/Users-Profile-Collection-/static/media/logo.8c08a809.png"
+  }
+]);
